2b5ea85c60aa222e29e2448595812fd3cb5fe344
kaushik chaudhary <kaushikbchaudhary@gmail.com>
2022-06-21 21:19:46 +0530

    first commit

---
+++ crud_development/script.js
@@ -0,0 +1,6 @@
+const empCode = document.getElementById(empCode);
+const fullName = document.getElementById(fullName);
+
+const submitForm = document.getElementById(submit);
+
+document.getElementById(submit).addEventListener("click", function () {});
