82783a4594e09bf871cd9899bc5ab4c1615c5767
kaushik chaudhary <kaushikbchaudhary@gmail.com>
2022-06-22 18:20:02 +0530

    adding functionality

--- crud_development/script.js
+++ crud_development/script.js
@@ -1,6 +1,35 @@
-const empCode = document.getElementById(empCode);
-const fullName = document.getElementById(fullName);
+function onFormSubmit() {
+  let formData = readFormData();
+  insertNewRecord(formData);
+  resetForm();
+}
 
-const submitForm = document.getElementById(submit);
+function readFormData() {
+  let formData = {};
+  formData["fullName"] = document.getElementById("fullName").value;
+  formData["empCode"] = document.getElementById("empCode").value;
+  formData["city"] = document.getElementById("city").value;
+  console.log(formData);
+  return formData;
+}
 
-document.getElementById(submit).addEventListener("click", function () {});
+function insertNewRecord(data) {
+  let table = document
+    .getElementById("employeeList")
+    .getElementsByTagName("tbody")[0];
+  let newRow = table.insertRow(table.length);
+  cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.empCode;
+
+  cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.fullName;
+
+  cell3 = newRow.insertCell(2);
+  cell3.innerHTML = data.city;
+}
+
+function resetForm() {
+  document.getElementById("fullName").value = "";
+  document.getElementById("empCode").value = "";
+  document.getElementById("city").value = "";
+}
