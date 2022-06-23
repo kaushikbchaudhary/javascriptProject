30cb2702c24a5f2ebb592e0dc91df84ee9801e95
kaushik chaudhary <kaushikbchaudhary@gmail.com>
2022-06-23 18:54:52 +0530

    Update & Delete

--- crud_development/script.js
+++ crud_development/script.js
@@ -1,6 +1,9 @@
+let selectedRow = null;
+
 function onFormSubmit() {
   let formData = readFormData();
-  insertNewRecord(formData);
+  if (selectedRow == null) insertNewRecord(formData);
+  else updateRecord(formData);
   resetForm();
 }
 
@@ -26,10 +29,36 @@ function insertNewRecord(data) {
 
   cell3 = newRow.insertCell(2);
   cell3.innerHTML = data.city;
+
+  cell4 = newRow.insertCell(3);
+  cell4.innerHTML = `<a onClick="onEdit(this)">Update</a><a onClick="onDelete(this)">Delete</a>`;
 }
 
 function resetForm() {
   document.getElementById("fullName").value = "";
   document.getElementById("empCode").value = "";
   document.getElementById("city").value = "";
+  selectedRow = null;
+}
+
+function onEdit(td) {
+  selectedRow = td.parentElement.parentElement;
+  document;
+  document.getElementById("empCode").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("fullName").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("city").value = selectedRow.cells[2].innerHTML;
+}
+
+function updateRecord(formData) {
+  selectedRow.cells[0].innerHTML = formData.empCode;
+  selectedRow.cells[1].innerHTML = formData.fullName;
+  selectedRow.cells[2].innerHTML = formData.city;
+}
+
+function onDelete(td) {
+  if (confirm("Are you sure to delete this record ?")) {
+    row = td.parentElement.parentElement;
+    document.getElementById("employeeList").deleteRow(row.rowIndex);
+    resetForm();
+  }
 }
